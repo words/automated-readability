0e2b752b2ea8b2ca452fc872646b4f3f0ff9efba
Titus Wormer <tituswormer@gmail.com>
2022-11-01 13:31:03 +0100

    Add docs to types

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+/**
+ * @typedef Counts
+ *   Counts from input document.
+ * @property {number} sentence
+ *   Number of sentences.
+ * @property {number} word
+ *   Number of words.
+ * @property {number} character
+ *   Number of characters.
+ */
+
+/**
+ * @typedef {Counts} AutomatedReadabilityCounts
+ *   Deprecated: please use the `Counts` type instead.
+ */
+
 const characterWeight = 4.71
 const sentenceWeight = 0.5
 const base = 21.43
 
 /**
- * @typedef {Object.<string, number>} AutomatedReadabilityCounts
- * @property {number} sentence
- * @property {number} word
- * @property {number} character
- */
-
-/**
- * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of characters  (`character`) in a document, returns the grade level associated with the document.
+ * Given an object containing the number of words (`word`), the number of
+ * sentences (`sentence`), and the number of characters  (`character`) in a
+ * document, returns the grade level associated with the document.
  *
- * @param {AutomatedReadabilityCounts} counts
+ * @param {Counts} counts
+ *   Counts from input document.
  * @return {number}
+ *   Grade level associated with the document.
  */
 export function automatedReadability(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.character) {
